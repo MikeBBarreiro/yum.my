ca5d66d6120922919165013704cbae2d25da9e9d
Michael Barreiro <Mikeb_barreiro@yahoo.com>
2014-09-15 10:59:32 -0500

    toastr

--- client/views/register/register.js
+++ client/views/register/register.js
@@ -1,12 +1,20 @@
+
 (function(){
   'use strict';
 
   angular.module('yummy')
-  .controller('RegisterCtrl', ['$scope', 'Register', function($scope, Register){
+  .controller('RegisterCtrl', ['$scope', 'User', function($scope, User){
     $scope.user = {};
 
+    function success(response){
+      toastr.success('Wonderful, You have registered!');
+    }
+    function failure(response){
+      toastr.error('ERROR, regrestration, please fix, or try again!');
+    }
+
     $scope.register = function(){
-      $scope.user = {};
+      User.register($scope.user).then(success, failure);
     };
   }]);
 })();
